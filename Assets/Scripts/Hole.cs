177f42c6038ec10ef4d2e1f95b71a9245c167941
deweytyl <ty.dewey@gmail.com>
2016-04-13 20:29:08 -0500

    Change hole to respawn at RespawnPoint

--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -2,19 +2,33 @@
 using System.Collections;
 
 public class Hole : MonoBehaviour {
-	public float x;
-	public float y;
-	void OnTriggerEnter2D (Collider2D other) {
+	
+	public GameObject respawnPoint;
+
+	void Update() {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.name == "Player") {
 			GameObject player = other.gameObject;
 
-			player.transform.position = new Vector3(x, y, 0);
+			player.transform.position = respawnPoint.transform.position;
+
+			// must be called after resetting player.transform.position
+			// something to fix in the future
 			GridMovement movement = player.GetComponent<GridMovement> ();
 			movement.ClearDestination ();
-		}
-
-		if (other.gameObject.name == "Clone") {
+		
+		} else if (other.gameObject.name == "Clone") {
 			Destroy (other.gameObject);
+		
+		} else if (other.gameObject.name == "CloneSpawner") {
+
 		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+
 	}
 }
