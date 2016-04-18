90ab74f58c7e1fd42bce85b34c85b40334144941
Jianting Chen <JiantingChen@Jiantings-MacBook-Pro.local>
2016-04-18 14:40:45 -0500

    craked holes

---
+++ Assets/Scripts/FakeFloor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FakeFloor : MonoBehaviour {
+
+	public Hole hole;
+
+	void Start () {
+		hole.active = false;
+	}
+
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.name == "Player" || other.name == "Clone") {
+			hole.active = true;
+			gameObject.SetActive (false);
+		}
+	}
+
+}
--- Assets/Scripts/Hole.cs
+++ Assets/Scripts/Hole.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class Hole : MonoBehaviour {
 	public float x;
 	public float y;
+	public bool active = true;
 	void OnTriggerEnter2D (Collider2D other) {
-		if (other.gameObject.name == "Player") {
-			GameObject player = other.gameObject;
+		if (active) {
+			if (other.gameObject.name == "Player") {
+				GameObject player = other.gameObject;
 
-			player.transform.position = new Vector3(x, y, 0);
-			GridMovement movement = player.GetComponent<GridMovement> ();
-			movement.ClearDestination ();
-		}
+				player.transform.position = new Vector3 (x, y, 0);
+				GridMovement movement = player.GetComponent<GridMovement> ();
+				movement.ClearDestination ();
+			}
 
-		if (other.gameObject.name == "Clone") {
-			Destroy (other.gameObject);
+			if (other.gameObject.name == "Clone") {
+				Destroy (other.gameObject);
+			}
 		}
 	}
 }
