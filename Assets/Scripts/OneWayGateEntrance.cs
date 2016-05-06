5db2c8e1a0cdd68d2587ec5faa1c4f55a2a9ab92
deweytyl <ty.dewey@gmail.com>
2016-05-06 09:01:35 -0500

    Add one way gates

---
+++ Assets/Scripts/OneWayGateEntrance.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayGateEntrance : MonoBehaviour {
+
+	public BoxCollider2D gateCollider; 
+
+	void OnTriggerEnter2D(Collider2D other) {
+		OpenGate ();
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		CloseGate ();
+	}
+
+	void OpenGate() {
+		gateCollider.enabled = false;
+	}
+
+	void CloseGate() {
+		gateCollider.enabled = true;
+	}
+}
---
+++ Assets/Scripts/OneWayGateOpener.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneWayGateOpener : MonoBehaviour {
+
+	public GameObject gateWall; 
+
+	void OnTriggerEnter2D(Collider2D other) {
+		gateWall.SetActive (false);
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		gateWall.SetActive (true);
+	}
+}
