a46ab4fc347e6d813eea4268c1a3df675a5572f6
deweytyl <ty.dewey@gmail.com>
2016-05-08 18:49:37 -0500

    Add Keyed gates

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	private Vector3 originalPosition;
+	private GameObject keyCarrier;
+
+	// Public methods
+
+	public void Drop() {
+		RemoveFromCarrier ();
+		transform.position = GridUtil.RoundToNearestHalf (transform.position);
+	}
+
+	public void Reset() {
+		RemoveFromCarrier ();
+		transform.position = originalPosition;
+	}
+
+	public void Destroy() {
+		Destroy (gameObject);
+	}
+
+	// Unity Lifecycle methods
+
+	void Start() {
+		originalPosition = transform.position;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Player") || other.CompareTag ("Clone")) {
+			keyCarrier = other.gameObject;
+			FollowCarrier ();
+			ResizeKey ();
+		}
+	}
+
+	// Helper methods
+
+	void FollowCarrier() {
+		transform.parent = keyCarrier.transform;
+		transform.localPosition = Vector3.zero;
+	}
+
+	void ResizeKey () {
+		transform.localScale = new Vector3 (0.75f, 0.75f);
+	}
+
+	void RemoveFromCarrier() {
+		transform.parent = null;
+		keyCarrier = null;
+	}
+		
+}
---
+++ Assets/Scripts/KeyedGate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyedGate : MonoBehaviour {
+
+	public GameObject gateRoot;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.CompareTag ("Key")) {
+			other.GetComponent<Key> ().Destroy ();
+			Destroy (gateRoot);
+		}
+	}
+}
