2a1eb24f64294d305277dde118bee05b9e34be81
deweytyl <ty.dewey@gmail.com>
2016-05-09 14:54:38 -0500

    Add Teleporters

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public Teleporter destination;
+
+	private bool shouldTeleport = true;
+
+	public void PrepareForLanding() {
+		shouldTeleport = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (shouldTeleport && 
+			(other.CompareTag ("Player") || other.CompareTag ("Clone"))) {
+			destination.PrepareForLanding ();
+			other.transform.position = destination.transform.position;
+			other.GetComponent<GridMovement> ().ClearDestination ();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		shouldTeleport = true;
+	}
+
+}
