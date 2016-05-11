79a5c3dc6365997a81970f64f252a95379af4bdc
Jianting Chen <JiantingChen@Jiantings-MacBook-Pro.local>
2016-05-11 15:24:09 -0500

    emergency class

--- Assets/Scripts/RoomGenerator/RoomGeneratorEditor.cs
+++ Assets/Scripts/RoomGenerator/RoomGeneratorEditor.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using System.Collections;
 using UnityEditor;
 
@@ -19,3 +20,4 @@ public class RoomGeneratorEditor : Editor {
 		DrawDefaultInspector ();
 	}
 }
+#endif
