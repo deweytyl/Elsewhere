db7d4f40f61e7b21faf228cb9015451b6c55e50e
deweytyl <ty.dewey@gmail.com>
2016-05-18 22:53:01 -0500

    Create background music player

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameStateManager : PersistentSingleton {
+
+	public static GameStateManager manager; // singleton instance
+
+	public string gameFinishedSceneName;
+
+	bool completedIceRooms = false;
+	bool completedTeleRooms = false;
+
+	void Awake() {
+		base.Awake ();
+		manager = this;
+	}
+
+	public void CompleteIceRooms() {
+		completedIceRooms = true;
+		CheckForGameCompletion ();
+	}
+
+	public void CompleteTeleRooms() {
+		completedTeleRooms = true;
+		CheckForGameCompletion ();
+	}
+
+	void CheckForGameCompletion() {
+		if (completedIceRooms && completedTeleRooms) {
+			SceneManager.LoadScene (gameFinishedSceneName);
+		}
+	}
+}
---
+++ Assets/Scripts/PersistentSingleton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersistentSingleton : MonoBehaviour {
+
+	public static PersistentSingleton instance;
+
+	protected void Awake () {
+		if (instance == null) {
+			DontDestroyOnLoad(gameObject);
+			instance = this;
+
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+	}
+}
